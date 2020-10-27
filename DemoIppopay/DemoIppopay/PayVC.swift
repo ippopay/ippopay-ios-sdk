03fdd5cd73a323ef6872dcae40a5d257ffb9e065
sathiydeva <>
2020-10-27 13:46:52 +0530

    Initial version

---
+++ DemoIppopay/DemoIppopay/PayVC.swift
@@ -0,0 +1,64 @@
+
+
+import UIKit
+import IppoPay
+
+class PayVC: UIViewController {
+    
+    @IBOutlet weak var stackViewBottomConstraints: NSLayoutConstraint!
+    @IBOutlet weak var amountTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(handelingKeyboard), name:UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handelingKeyboard), name:UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func errorAlert(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    @IBAction func payButtonTapped(_ sender: Any) {
+        let amount = amountTextField.text
+        if let value = Double(amount ?? "0.0") {
+            if amount == "" || value <= 0 {
+                errorAlert(message: "Please enter valid amount")
+            } else {
+                Ippopay.initSDK(publicKey: "PUBLIC_KEY", withDelegate: self)
+                let orderData = OrderData()
+                orderData.orderId = "ORDER_ID"
+                Ippopay.makePayment(orderData: orderData)
+                PayLog.setLogVisible(debug: true)
+            }
+        }
+    }
+    
+    @objc func handelingKeyboard(notification: Notification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            let isKeyboardShow = notification.name == UIResponder.keyboardWillShowNotification
+            stackViewBottomConstraints.constant =  isKeyboardShow ? keyboardSize.height : 0
+            UIView.animate(withDuration: 0.0, delay: 0.0, options: .curveEaseOut, animations: {
+                self.view.layoutIfNeeded()
+            }) { (isSuccess) in
+            }
+        }
+    }
+
+}
+
+extension PayVC: IppopayDelegate {
+    
+    func onPaymentError(descriptionOfError: String) {
+        print("DescriptionError:::::::::::\(descriptionOfError)")
+    }
+    
+    func onPaymentSuccess(paymentId: String) {
+        print("TransactionID:::::::::\(paymentId)")
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,98 @@
-# ippopay-ios-sdk
-Ippopay iOS SDK
+# IppoPay iOS SDK Integration
+
+## Step - 1 - Add Dependency
+
+```
+pod 'IppoPay', '~> 1.0.0'
+pod update
+```
+
+## Step - 2 - Import SDK 
+
+```
+import IppoPay
+```
+
+
+## Step - 3 - Initialize SDK 
+
+To initialize the SDK add the below line of code with the public key you retrieved from ippopay merchant panel. If you don't have a public key create new one.
+
+```
+Ippopay.initSDK("Your Unique Public Key",withDelegate: Self)
+```
+
+## Step - 4 - Create Order Data Object with necessary inputs
+
+You can create the order data or payment input with our OrderData Model class. Here you need to provide order id and customer details like name, email, mobile number.
+
+```
+let orderData = OrderData()
+orderData.customColor = "#12233"  // make payment page loading color as app color. 
+orderData.orderId = ""  // unique order id. 
+orderData.orderDescription = ""  // any description.
+let customer = Customer()
+customer.customerEmail = "email@gmail.com"
+customer.customerName = "name"
+customer.customerPhoneNumber = "1234567890"
+orderData.customer = customer
+
+```
+
+## Step - 5 - Make Transaction with Ippopay
+
+Use the below line of code to make the payment with the order data you created in Step - 4
+
+```
+
+Ippopay.makePayment(orderData: orderData)
+
+```
+
+## Step - 6 - Implement Payment Delegate
+
+Implement our payment delegate to receive the payment result for the payment we made in Step - 5. Use the below code to obtain the payment result.
+
+```
+
+extension ViewController: IppopayDelegate{
+    func onPaymentError(descriptionOfError: String) {
+        // Failure Callback.
+    }
+    
+    func onPaymentSuccess(paymentId: String) {
+         // Success Callback
+    }
+}
+
+```
+
+## iOS 9 Update
+
+iOS 9 has higher requirements for secure URLs. As many Indian banks do not comply with the requirements, you can implement the following as a workaround in Info.plist
+
+```
+
+<key>NSAppTransportSecurity</key>
+<dict>
+    <key>NSAllowsArbitraryLoads</key>
+    <true/>
+</dict>
+
+```
+
+## Ippopay Log
+
+You can enable / disable the SDK logs by using the below line of code. By default it will be enabled.
+
+```
+PayLog.setLogVisible(debug: true or false)
+
+```
+
+
+
+## Sample Payment Reference.
+
+Please check [this link](https://github.com/ippopay/ippopay-ios-sdk/tree/master/Demo) for sample payment with above steps.
+
